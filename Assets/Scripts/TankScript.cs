74824556eb6206e854715ba7c68cfe6c555ec4cf
wangkai <1034832478@qq.com>
2019-08-04 11:14:31 +0800

    坦克使用四元数进行移动

--- Assets/Scripts/TankScript.cs
+++ Assets/Scripts/TankScript.cs
@@ -7,6 +7,7 @@ public class TankScript : MonoBehaviour
     public float moveSpeed;
     public float rotateAngle;
     public float rotateSpeed;
+    public AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,23 +19,48 @@ public class TankScript : MonoBehaviour
     {
         float v = Input.GetAxis("Vertical");
         float h = Input.GetAxis("Horizontal");
+
+        if (v != 0)
+        {
+            if (!this.audioSource.isPlaying) {
+                this.audioSource.Play();
+            }
+        }
+        else
+        {
+            this.audioSource.Stop();
+        }
+
         if (v > 0)
         {
+           
             this.gameObject.transform.Translate(new Vector3(moveSpeed, 0f, 0f), Space.Self);
         }
         else if (v < 0)
         {
+            this.audioSource.Play();
             this.gameObject.transform.Translate(new Vector3(-moveSpeed, 0f, 0f), Space.Self);
         }
+        else {
+            this.audioSource.Stop();
+        }
+
+
         if (h > 0)
         {
+            // 直接使用欧拉角进行旋转
             rotateAngle += rotateSpeed * Time.deltaTime;
-            this.gameObject.transform.eulerAngles = new Vector3(0, rotateAngle, 0);
+            // this.gameObject.transform.eulerAngles = new Vector3(0, rotateAngle, 0);
+            // 使用四元数方法进行旋转
+            this.gameObject.transform.rotation = Quaternion.Euler(0, rotateAngle, 0);
         }
         else if (h < 0)
         {
+            // 直接使用欧拉角进行旋转
             rotateAngle -= rotateSpeed * Time.deltaTime;
-            this.gameObject.transform.eulerAngles = new Vector3(0, rotateAngle, 0);
+            // this.gameObject.transform.eulerAngles = new Vector3(0, rotateAngle, 0);
+            // 使用四元数方法进行旋转
+            this.gameObject.transform.rotation = Quaternion.Euler(0, rotateAngle, 0);
         }
     }
 }
